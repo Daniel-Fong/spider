d613eaba6d75861c0af9e828540c472a5e019ee7
Daniel Fong <daniel.fong.mv@gmail.com>
2021-02-09 11:35:53 -0700

    commented http connection

--- src/HTMLParser.java
+++ src/HTMLParser.java
@@ -1,25 +1,37 @@
 import java.util.*;
-import java.io.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import java.io.IOException;
 
 public class HTMLParser {
 
+    /**
+     * parses links from html
+     * @param String html
+     * @return List<String> linksArr
+     */
     public static List<String> getLinks (String html) {
+        // jsoup parse on html
         Document document = Jsoup.parse(html);
+        // select hrefs from parsed html
         Elements links = document.select("a[href]"); 
         List<String> linksArr = new ArrayList<>();
+        // loop through links
         for (Element link : links) {
+            // stringify link
             String linkStr = link.attr("href").toString();
+            // if absolute link
             if (linkStr.contains("www.siliconmtn.com")) {
+                // format link
                 linkStr = linkStr.replaceFirst("http://", "");
                 linkStr = linkStr.replaceFirst("www.siliconmtn.com", "");
+                // add to linksArr
                 linksArr.add(linkStr);
             }
+            // if relative link
             else if (linkStr.charAt(0) == '/' && linkStr.length() > 1) {
+                // add to linksArr
                 linksArr.add(linkStr);
             }
         }
--- src/HTTPConnection.java
+++ src/HTTPConnection.java
@@ -1,28 +1,31 @@
-import java.util.*;
-
 import javax.net.ssl.SSLSocketFactory;
-
-import java.net.*;
 import java.net.Socket;
 import java.io.*;
 
 public class HTTPConnection {
-// 52.37.173.54:443
     
+    /**
+     * Connect and get page
+     * @param String link
+     * @return String html
+     */
     public String getWebPage(String link) {
-
+        // string builder for html
         StringBuilder html = new StringBuilder();
 
         try (Socket socket = SSLSocketFactory.getDefault().createSocket("www.siliconmtn.com", 443)) {
-            
+            // set up streams
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // write http request
             out.writeBytes("GET " + link + " HTTP/1.1 \n");
             out.writeBytes("Host: " + "www.siliconmtn.com" + "\n\n");
             String inData;
             String newLine = System.lineSeparator();
+            // loop while reader still has data to read
             while((inData = in.readLine()) != null && (in.ready()) ) {
+                // add html to string builder
                 html.append(inData + newLine);
             }
         } catch (Exception e) {
@@ -32,16 +35,20 @@ public class HTTPConnection {
     }
 
     public String postLogin() {
+        // string builder for html
         StringBuilder html = new StringBuilder();
 
         try (Socket socket = SSLSocketFactory.getDefault().createSocket("www.siliconmtn.com", 443)) {
-            
+            // set up streams
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // set request body
             String requestBody = "requestType=reqBuild&pmid=ADMIN_LOGIN&emailAddress=daniel.fong%40siliconmtn.com&password=SMTRul3s%21&l=";
+            // set content length header
             int contentLength = requestBody.length();
 
+            // write post http request with headers and body
             out.writeBytes("POST /sb/admintool HTTP/1.1 \n");
             out.writeBytes("Content-Type: application/x-www-form-urlencoded\r\n");
             out.writeBytes("Content-Length: " + contentLength + "\r\n");
@@ -50,7 +57,9 @@ public class HTTPConnection {
             
             String inData;
             String newLine = System.lineSeparator();
+             // loop while reader still has data to read
             while((inData = in.readLine()) != null && (in.ready()) ) {
+                // add html to string builder
                 html.append(inData + newLine);
             }
         } catch (Exception e) {
@@ -60,20 +69,23 @@ public class HTTPConnection {
     }
 
     public String getProtectedWebPage(String link, String cookies) {
-
+        // string builder for html
         StringBuilder html = new StringBuilder();
 
         try (Socket socket = SSLSocketFactory.getDefault().createSocket("www.siliconmtn.com", 443)) {
-            
+            // set up streams
             DataOutputStream out = new DataOutputStream(socket.getOutputStream());
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // write http get request with cookies to persist session
             out.writeBytes("GET " + link + " HTTP/1.1 \n");
             out.writeBytes("Host: " + "www.siliconmtn.com" + "\n");
             out.writeBytes("Cookie: " + cookies + "\n\n");
             String inData;
             String newLine = System.lineSeparator();
+             // loop while reader still has data to read
             while((inData = in.readLine()) != null && (in.ready()) ) {
+                // add html to string builder
                 html.append(inData + newLine);
             }
         } catch (Exception e) {
