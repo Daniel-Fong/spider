8fe2886b243f90b33421f666ec4ce1fd10dd4d34
Daniel Fong <daniel.fong.mv@gmail.com>
2021-02-09 11:26:37 -0700

    added comments to links array manager and saver

--- src/CookieSaver.java
+++ src/CookieSaver.java
@@ -1,8 +1,16 @@
 public class CookieSaver {
+    /**
+     * gets cookies from html
+     * @param String html
+     * @return String cookies
+     */
     public static String getCookies(String html) {
         String cookies = "";
+        // loop until cookies no longer present in html
         while (html.contains("Set-Cookie: ")) {
+            // set index of beginning of cookie
             int index = html.indexOf("Set-Cookie: ");
+            // set offset for part that we don't need
             int offset = ("Set-Cookie: ").length();
             int endIndex = html.indexOf(";", index) + 1;
             // take cookie from that index
--- src/LinksArrayManager.java
+++ src/LinksArrayManager.java
@@ -2,20 +2,35 @@ import java.util.*;
 
 public class LinksArrayManager {
 
+    /**
+     * creates queue
+     * @param String firstLink
+     * @return List<String> linksArr
+     */
     public static List<String> createLinksArr(String firstLink) {
         List<String> linksArr = new ArrayList<>();
+        // add first link to linksArr
         linksArr.add(firstLink);
         return linksArr;
     }
 
+    /**
+     * 
+     * @param String link
+     * @param List<String> linksArr
+     * @return
+     */
     public static Boolean inArray(String link, List<String> linksArr) {
+        // loop through links arr
         for (int i = 0; i<linksArr.size(); i++) {
+            // if in links arr, return true
             if (linksArr.get(i).equals(link)) {
                 return true;
             }
             else {
             }
         }
+        // if loop through entire array and not there, return false
         return false;
     }
 
